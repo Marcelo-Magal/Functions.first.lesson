3d20b7e32738a0671d81cd054c6405289806eece
Marcelo Carvalho <marcelomagal@gmail.com>
2023-06-27 19:18:58 -0300

    Refactoring the code

--- Functions1/functions01.js
+++ Functions1/functions01.js
@@ -2,87 +2,97 @@
 //6 - Faça com que seu usuário escolha qual função ele deseja fazer
 // com os números que ele inseriu, e depois de realizar essa operação
 // pergunte se ele deseja fazer outra ou encerrar a aplicação.
-var continuar = true;
-var contador = 0;
-var resultado = [];
+let continuar = true;
+let contador = 0;
+let resultado = [];
+let numeroA, numeroB;
+let resultadoDaOperacao;
+let operacao;
 
 while (continuar) {
-    var operacao = prompt("Digite a opção desejada: \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão ");
+    operacao = prompt("Digite a opção desejada: \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão ");
+    
+    PerguntarNumeros();
 
-    // 1 - Crie um algoritmo que solicite ao seu usuário dois números A e B.
-    var numeroA = parseInt(prompt("Qual é o número A?"));
-    var numeroB = parseInt(prompt("Qual é o número B?"));
+    RealizarOperacao();
+    
+    ContinuarOperacao();
+}
 
-    var resultadoDaOperacao;
+ExibirHistorico();
+
+// 1 - Crie um algoritmo que solicite ao seu usuário dois números A e B.
+function PerguntarNumeros () {
+    numeroA = parseInt(prompt("Qual é o número A?"));
+    numeroB = parseInt(prompt("Qual é o número B?"));
+}
+
+function RealizarOperacao() {
     switch (operacao) {
 
         case "1":
-            resultadoDaOperacao = SomarNumeros();
+            resultadoDaOperacao = SomarNumeros(numeroA, numeroB);
             break;
 
         case "2":
-            resultadoDaOperacao = SubtrairNumeros();
+            resultadoDaOperacao = SubtrairNumeros(numeroA, numeroB);
             break;
 
         case "3":
-            resultadoDaOperacao = MultiplicarNumeros();
+            resultadoDaOperacao = MultiplicarNumeros(numeroA, numeroB);
             break;
 
         case "4":
-            resultadoDaOperacao = DividirNumeros();
+            resultadoDaOperacao = DividirNumeros(numeroA, numeroB);
             break;
 
         default:
             console.log("Opção inválida. Tente novamente.");
             break;
     }
-    
     resultado[contador] = resultadoDaOperacao;
     contador++
+}
+
+function ContinuarOperacao() {
     //console.log("O resultado da operação é " + resultadoDaOperacao)
-    var continuarOperacao =  prompt("Deseja realizar outra operação?")
-        if (continuarOperacao.toUpperCase() === 'N' || continuarOperacao.toUpperCase() === 'NAO' || continuarOperacao.toUpperCase() === 'NÃO') {
+    let resposta =  prompt("Deseja realizar outra operação?")
+        if (resposta.toUpperCase() === 'N' || resposta.toUpperCase() === 'NAO' || resposta.toUpperCase() === 'NÃO') {
         continuar = false;
-    }  
+    }
 }
 
 function ExibirHistorico() {
     console.log("Histórico de resultados")
-    for (var i = 0; i < resultado.length; i++) {
+    for (let i = 0; i < resultado.length; i++) {
     console.log("Resultado " + (i + 1) + ": " + resultado[i]);
    }
 }
 
-ExibirHistorico();
-
 //2 - Crie uma função que receba os dois valores fornecidos pelo usuário
 // e faça uma soma com eles, e retorne o resultado;
 
-function SomarNumeros() {
-    var soma = (numeroA + numeroB);
-    return soma;
+function SomarNumeros(a, b) {
+    return a + b;
 }
 
 //3 - Crie uma função que receba os dois valores fornecidos pelo usuário
 // e faça uma subtração com eles, e retorne o resultado;
 
-function SubtrairNumeros() {
-    var subtracao = (numeroA - numeroB);
-    return subtracao;
+function SubtrairNumeros(a, b) {
+    return a - b;
 }
 
 //4 - Crie uma função que receba os dois valores fornecidos pelo usuário
 // e faça uma multiplicação com eles, e retorne o resultado;
 
-function MultiplicarNumeros() {
-    var multiplicacao = (numeroA * numeroB);
-    return multiplicacao;
+function MultiplicarNumeros(a, b) {
+    return a * b;
 }
 
 //5 - Crie uma função que receba os dois valores fornecidos pelo usuário
 // e faça uma divisão com eles, e retorne o resultado;
 
-function DividirNumeros() {
-    var divisao = (numeroA / numeroB);
-    return divisao;
+function DividirNumeros(a, b) {
+    return a / b;
 }

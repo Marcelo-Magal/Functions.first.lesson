219aaed5f4b0454d063c3b30ff2f87e3ba971e32
Marcelo Carvalho <marcelomagal@gmail.com>
2023-06-27 20:20:46 -0300

    Creating a function that asks user for a name and a password

---
+++ Functions3/functions31.js
@@ -0,0 +1,9 @@
+let nome;
+let senha;
+
+//1 - Crie uma função que solicite dois valores ao usuário,
+// um nome e uma senha e guarde cada um no seu respectivo array.
+function SolicitarDados() {
+    nome = prompt("Qual é o nome?");
+    senha = prompt("Qual é a senha?");
+}

06039fa7845bedff21885a6e673440d69301c801
Marcelo Carvalho <marcelomagal@gmail.com>
2023-06-27 21:49:04 -0300

    Creating login function

--- Functions3/functions31.js
+++ Functions3/functions31.js
@@ -1,12 +1,17 @@
 let nome;
 let senha;
+let nomes = [];
+let senhas = [];
+let contador = 0;
 
-PerguntarAcao();
 //1 - Crie uma função que solicite dois valores ao usuário,
 // um nome e uma senha e guarde cada um no seu respectivo array.
 function SolicitarDados() {
     nome = prompt("Qual é o nome?");
     senha = prompt("Qual é a senha?");
+    nomes[contador] =  nome;
+    senhas[contador] = senha;
+    contador++;
 }
 
 //2 - Crie uma função que solicite ao seu usuário o que ele deseja fazer:
@@ -16,3 +21,26 @@ function PerguntarAcao() {
     let resposta = prompt("Qual operação deseja /n realizar?" + "1 - Cadastrar." + "2 - Fazer login." + "3 - Excluir um cadastro." + "4 - Encerrar o programa");
     return resposta;
 } 
+
+//3 - Crie uma função de login e recebe dois valores como parâmetro,
+// um nome e uma senha, depois procure o nome recebido no array de nomes
+// e em caso positivo compare a senha no array de senhas. Se as duas
+// comparações forem válidas retorne true se uma delas não for válida retorne false.
+function FazerLogin() {
+    let nomeLogin = prompt("Digite o nome de login:");
+    let senhaLogin = promt("Qual a semnhade login:");
+    let loginEfetuado = false;
+    for (i = 0; i < nomes.length; i++) {
+        if (nomeLogin == nomes[i]) {
+            if (senhaLogin == senhas[i]) {
+                loginEfetuado = true;
+                console.log("login efetuado com sucesso.")
+            }
+        }
+        else (loginEfetuado == false) {
+            console.log("Nome de usuário ou senha incorretos");
+        }
+    }
+
+
+}
